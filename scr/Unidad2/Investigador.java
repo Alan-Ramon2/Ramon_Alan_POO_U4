67cf43775a96764bb509e0dfad529ed4ddc97973
Alan-Ramon2 <aramonp1@est.ups.edu.ec>
2026-05-03 22:17:39 -0500

    Create Investigador.java

---
+++ scr/Unidad2/Investigador.java
@@ -0,0 +1,13 @@
+package Unidad2;
+
+public class Investigador {
+    private String nombre;
+
+    public Investigador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
